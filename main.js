4c59c2fa9c8d95421f01bca9125d5a81bf7fa2ec
kimmckenzie76-jpg <kim.mckenzie76@gmail.com>
2026-02-12 17:15:59 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// This variable stores the first number
+var num1 = 10;
+
+// This variable stores the second number
+var num2 = 5;
+
+// This expression adds the two numbers together
+var result = num1 + num2;
+
+// This line displays the result on the webpage
+document.write("The result is: " + result);
+
+
+
+
+
+
+
+
