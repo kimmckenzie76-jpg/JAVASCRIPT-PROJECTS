0f9f3046ccde927a5c8dc1eb587536b1b2192a6a
kimmckenzie76-jpg <kim.mckenzie76@gmail.com>
2026-02-17 15:46:31 -0800

    Added String VAR

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+window.alert("Hello World")
+// This variable stores the first number
+var num1 = 10;
+
+// This variable stores the second number
+var num2 = 5;
+
+// This expression adds the two numbers together
+var result = num1 + num2;
+
+// This line displays the result on the webpage
+document.write("The result is: " + result);
+
+var food  = "strawberry, blackberry, blueberry"
+var meat  = "steak, hamburger, fish"
+document.write (food + meat + "2 parts of your daily food groups")
+
+
+
+
+
