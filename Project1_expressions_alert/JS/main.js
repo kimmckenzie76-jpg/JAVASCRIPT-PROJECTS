105e4945ca4532212a4a50e3adbf6a89c75ee6a3
kimmckenzie76-jpg <kim.mckenzie76@gmail.com>
2026-02-18 15:55:32 -0800

    Update to JS File and Index HTML

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,4 +1,5 @@
-window.alert("Hello World")
+window.alert("Hello World");
+
 // This variable stores the first number
 var num1 = 10;
 
@@ -9,13 +10,24 @@ var num2 = 5;
 var result = num1 + num2;
 
 // This line displays the result on the webpage
-document.write("The result is: " + result);
+document.write("The result is: " + result + "<br>");
 
-var food  = "strawberry, blackberry, blueberry"
-var meat  = "steak, hamburger, fish"
-document.write (food + meat + "2 parts of your daily food groups")
+// These variables store food groups
+var food = "strawberry, blackberry, blueberry";
+var meat = "steak, hamburger, fish";
 
+// This displays the food groups
+document.write(food + " and " + meat + " are 2 parts of your daily food groups.<br>");
 
+// This function uses the += operator to add numbers together
+function addNumbers() {
 
+    // Create a variable and assign it a starting value
+    let total = 5;
 
+    // Use the += operator to add 10 to total
+    total += 10;
 
+    // Display the result inside the HTML element with id="result"
+    document.getElementById("result").innerHTML = "The total is: " + total;
+}
